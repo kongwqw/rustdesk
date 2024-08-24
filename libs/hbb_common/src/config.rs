1a96cf19b6c0d9145b2c485d33148e6eb04253fe
kongwqw <71058285+kongwqw@users.noreply.github.com>
2024-08-24 13:37:47 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.kongwqw.top"];
+pub const PUBLIC_RS_PUB_KEY: &str = "NDzk4a3sDH5cPPiSqbPfdEOV6sx2fTWqGIkGfGam8Ho=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
