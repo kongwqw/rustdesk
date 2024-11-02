29133aef668c0fa026c498d161e1472f3c9a0e69
kongwqw <71058285+kongwqw@users.noreply.github.com>
2024-11-02 16:55:11 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,7 +100,7 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["www.kongwqw.top"];
+pub const RENDEZVOUS_SERVERS: &[&str] = &["www.790103.xyz"];
 pub const PUBLIC_RS_PUB_KEY: &str = "SB8Bbs2dAzKXa2GlkcuUgRutuI7Mu9o5eBPhMyVMP28=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
